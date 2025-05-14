aa408099e0e0bf5fdaa80fbe67b11bdd83729fa5
kugukugu <n-a-v-e-r_is_a_firm@naver.com>
2025-05-14 21:11:56 +0900

    Update Linked_List.c

--- Linked_List.c
+++ Linked_List.c
@@ -21,11 +21,21 @@ struct Node* create_node(char* data)
 // 전달받은 node에 대해 메모리 해제
 void delete_node(struct Node* node)
 {
+    free (node->data);
+    free(node);
 }
 
 // 전달받은 head 를 이용해 연결리스트의 모든 노드에 대해서 메모리 해제
 void delete_all(struct Node* head)
 {
+    struct Node* temp;
+
+    while(head != NULL)
+    {
+        temp = head->next;
+        delete_node(head);
+        head = temp;
+    }
 }
 
 // 전달받은 head를 이용해 연결리스트의 맨 끝에 data 노드를 추가
@@ -59,11 +69,57 @@ int add_head(struct Node** head, char* data)
 // 전달받은 head를 이용해 연결리스트의 값 중 target_data 노드의 다음에 data 노드를 추가
 int add_next(struct Node** head, char* target_data, char* data)
 {
+    struct Node* temp = *head;
+
+    while(temp  != NULL)
+    {
+        if(strcmp(temp->data, target_data)==0)
+        {
+            break;
+        }
+        temp= temp->next;
+    }
+
+    if(temp==NULL)
+    {
+        return 0;
+    }
+
+    struct Node* new1= create_node(data);
+
+    new1->next=temp->next;
+    temp->next=new1;
+
+    return 1;
 }
 
 // 전달받은 head를 이용해 연결리스트의 i번째 노드 다음에 data 노드를 추가
-int add_index(struct Node** head, int i, char* data)
+int add_index(struct Node** head, int index, char* data)
 {
+    struct Node *temp = *head;
+    int count = 0;
+
+    while(temp != NULL)
+    {
+        if(count == index)
+        {
+            break;
+        }
+        temp = temp ->next ;
+        count ++;
+    }
+
+    if(temp==NULL)
+    {
+        return 0;
+    }
+
+    struct Node* new1= create_node(data);
+
+    new1->next=temp->next;
+    temp->next=new1;
+
+    return 1;
 }
 
 // 전달받은 head를 이용해 연결리스트 전체 노드의 data를 보기좋게 출력
@@ -74,25 +130,107 @@ void print_linkedlist(struct Node* head)
         printf("%s >> ", head->data);
         head = head->next;
     }
+    printf("\n");
+}
+
+int delete_target(struct Node** head, char* target_data)
+{
+    struct Node* temp = *head;
+    struct Node* prev_temp = NULL;
+
+    while(temp  != NULL)
+    {
+        if(strcmp(temp->data, target_data)==0)
+        {
+            break;
+        }
+        prev_temp = temp;
+        temp= temp->next;
+    }
+
+    if(temp==NULL)
+    {
+        return 0;
+    }
+
+    if(prev_temp == NULL)
+    {
+        *head = temp->next;
+    }
+    else
+    {
+        prev_temp->next = temp->next;
+    }
+    delete_node(temp);
+
+    return 1;
+}
+
+int delete_index(struct Node** head, int index)
+{
+    struct Node* temp = *head;
+    struct Node* prev_temp = NULL;
+    int count = 0;
+
+    while(temp  != NULL)
+    {
+        if(count == index)
+        {
+            break;
+        }
+        prev_temp = temp;
+        temp= temp->next;
+        count ++;
+    }
+
+    if(temp==NULL)
+    {
+        return 0;
+    }
+
+    if(prev_temp == NULL)
+    {
+        *head = temp->next;
+    }
+    else
+    {
+        prev_temp->next = temp->next;
+    }
+    delete_node(temp);
+
+    return 1;
 }
 
 int main()
 {
-  struct Node* head = NULL;
+    struct Node* head = NULL;
 
-  // 연결리스트의 맨 앞에 hello 추가
-  add_head(&head, "hello");
+    // 연결리스트의 맨 앞에 hello 추가
+    add_head(&head, "hello");
 
-  // 연결리스트의 맨 뒤에 world 추가
-  add_tail(&head, "world");
+    // 연결리스트의 맨 뒤에 world 추가
+    add_tail(&head, "world");
 
-  // 연결리스트의 hello 뒤에 good 추가
+    // 연결리스트의 hello 뒤에 good 추가
+    add_next(&head, "hello", "good");
 
-  // 연결리스트의 2번째 뒤에 bad 추가
+    // 연결리스트의 2번째 뒤에 bad 추가
+    add_index(&head, 2, "bad");
 
-  // 전체 연결리스트 출력
+    // 연결리스트에서  world 삭제
+    delete_target(&head, "world");
+
+    // 연결리스트에서  1번 째 삭제
+    delete_index(&head, 1);
+
+    // 전체 연결리스트 출력
     print_linkedlist(head);
-  // 전체 연결리스트 삭제
+
+    // 전체 연결리스트 삭제
+    delete_all(head);
+
+    // 연결리스트 삭제 확인
+    print_linkedlist(head);
 
 	return 0;
 }
